44d54f77a6e2e5fbccd25b6fe6282f19d8051cf8
ccc <webdesign.up2cc3@gmail.com>
2017-05-30 19:15:28 +1200

    Initial commit

---
+++ Ficheros.java
@@ -0,0 +1,82 @@
+
+
+/**
+ * Created by ccc on 30/05/2017.
+ */
+import org.ghost4j.document.PDFDocument;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ccc on 27/05/2017.
+ */
+public class Ficheros {
+
+    private String result;
+    private File imageFile;
+    private PDFDocument pdf;
+    public String carpetaInicial = "C:\\Users\\ccc\\Documents\\Tesseract\\Unsorted";
+
+
+    public Ficheros(String result, File imageFile, PDFDocument pdf) {
+
+        setResult(result);
+        setImageFile(imageFile);
+        setPdf(pdf);
+
+    }
+
+    public List<Ficheros>buscarFicheros() {
+        List<Ficheros> ficheros = new ArrayList<Ficheros>();
+        PDFDocument pdf=new PDFDocument();
+
+        File fDirectorio = new File(carpetaInicial);
+        if (fDirectorio.isDirectory()) {
+            File[] myFiles = fDirectorio.listFiles();
+
+            for (File myFile : myFiles) {
+
+                if (myFile.getName().contains(".pdf")) {
+                    try{
+                        pdf.load(myFile);    } catch (Exception e) {
+                        System.out.println("ERROR: " + e.getMessage());
+                    }
+                    ficheros.add(new Ficheros(null, myFile,pdf));
+
+                }
+            }
+        }
+        return ficheros;
+    }
+
+
+
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public File getImageFile() {
+        return imageFile;
+    }
+
+    public void setImageFile(File imageFile) {
+        this.imageFile = imageFile;
+    }
+
+    public PDFDocument getPdf() {
+        return pdf;
+    }
+
+    public void setPdf(PDFDocument pdf) {
+        this.pdf = pdf;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+}
+
---
+++ SortScannedJobCards.java
@@ -0,0 +1,86 @@
+
+
+/**
+ * Created by ccc on 30/05/2017.
+ */
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+import org.apache.log4j.BasicConfigurator;
+import org.ghost4j.document.Document;
+import org.ghost4j.document.DocumentException;
+import org.ghost4j.document.PDFDocument;
+import org.ghost4j.modifier.ModifierException;
+import org.ghost4j.modifier.SafeAppenderModifier;
+
+import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class SortScannedJobCards {
+
+
+    private static Rectangle area = new Rectangle(
+            2060, 20, 300, 300);
+
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+        String carpetaFinal = "C:\\Users\\ccc\\Documents\\Tesseract\\Sorted\\";
+        String result = null;
+        File imageFile = null;
+        PDFDocument pdf = null;
+        Ficheros ficheros = new Ficheros(result, imageFile, pdf);
+
+        java.util.List<Ficheros> listaFicheros = ficheros.buscarFicheros();
+        Tesseract instance = new Tesseract(); // JNA Interface Mapping
+        // Tesseract1 instance = new Tesseract1(); // JNA Direct Mapping
+        for (Ficheros f : listaFicheros)
+            try {
+                String resultado = instance.doOCR(f.getImageFile(), area);
+                if (resultado != null) {
+                    resultado = resultado.substring(0, 5);
+                    System.out.println(resultado);
+
+                    File newfile = new File(carpetaFinal + resultado + ".pdf");
+                    if (newfile.exists()) {
+                        PDFDocument newpdf = new PDFDocument();
+                        newpdf.load(new File(carpetaFinal + resultado + ".pdf"));
+                        PDFDocument myPdf = new PDFDocument();
+                        myPdf.load(f.getImageFile());
+                        SafeAppenderModifier modifier = new SafeAppenderModifier();
+                        Map<String, Serializable> parameters = new HashMap<String, Serializable>();
+                        parameters.put(SafeAppenderModifier.PARAMETER_APPEND_DOCUMENT, myPdf);
+                        Document apennd = modifier.modify(newpdf, parameters);
+                        apennd.write(new File(carpetaFinal + resultado + ".pdf"));
+                        f.getImageFile().delete();
+
+                    } else {
+
+
+                        if (f.getImageFile().renameTo(newfile)) {
+                            System.out.println("Rename succesful");
+                        } else {
+                            System.out.println("Rename failed");
+                        }
+                    }
+                }
+
+            } catch (TesseractException e) {
+                System.err.println(e.getMessage());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (DocumentException e) {
+                e.printStackTrace();
+            } catch (ModifierException e) {
+                e.printStackTrace();
+            }
+    }
+
+}
